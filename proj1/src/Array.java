c8620883cab9a6166ff94ed57e0db68e31159934
taiyang-1 <2107502278@qq.com>
2026-05-25 01:07:07 +0800

    数组

---
+++ proj1/src/Array.java
@@ -0,0 +1,36 @@
+/**
+ * @author lyf
+ */
+public class Array {
+    public static void main(String[] args) {
+        int array[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = (int)(Math.random()*10+1);
+        }
+//        找最大值
+        int max = array[0];
+        for (int i = 0; i < array.length-1 ; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+//        找最小值
+        int min = array[0];
+        for (int i = 0; i < array.length-1 ; i++) {
+            if (array[i]<min)
+                min = array[i];
+        }
+//        平均数
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+        System.out.println("最大值="+max);
+        System.out.println("最小值="+min);
+        System.out.println("总和="+sum);
+        System.out.println("平均数="+sum/10);
+    }
+}
