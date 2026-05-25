0320c885a59e38a8e55af3e180807ae43b1b1102
taiyang-1 <2107502278@qq.com>
2026-05-25 10:14:31 +0800

    增加斐波那契算法

--- proj1/src/Recursion.java
+++ proj1/src/Recursion.java
@@ -11,12 +11,30 @@ public class Recursion {
         int n = scanner.nextInt();
         int result = factorial(5);
         System.out.println(result);
+
+        System.out.println("(fib)请输入数字: ");
+        int m = scanner.nextInt();
+        int result2 = fib(m);
+        System.out.println(result2);
+
+
     }
     public static int factorial(int n ){
         if(n==1){
             return 1;
         }
         return n*factorial(n-1);
+    }
+
+    public static int fib(int m ){
+        if(m==0){
+            return 0;
+        }else if(m==1){
+            return 1;
+        }else{
+            return fib(m-1)+fib(m-2);
+        }
+
     }
 
 
