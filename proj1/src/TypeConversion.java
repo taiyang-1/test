f6e781f7e1ad9c5a6e2b04d66d6aad3eed58171a
taiyang-1 <2107502278@qq.com>
2026-05-25 16:51:53 +0800

    熟悉String

--- proj1/src/TypeConversion.java
+++ proj1/src/TypeConversion.java
@@ -5,23 +5,34 @@
 public class TypeConversion {
     public static void main(String[] args) {
 
-        int a = 1;
-        double b = 3.13;
-        char c = 'a';
-        boolean d = true;
+//        int a = 1;
+//        double b = 3.13;
+//        char c = 'a';
+//        boolean d = true;
+//
+//
+//        //自动转换
+//        double conversionA = a;
+//        System.out.println(conversionA);
+//
+//        //强制转换
+//        int conversionB = (int)b;
+//        System.out.println(conversionB);
+//
+//       //char类型转换为int类型
+//
+//        int conversionC = (int)c;
+//        System.out.println(conversionC);
 
+        String str = "hello";
+        System.out.println(str.length());
+        System.out.println(str.charAt(2));
+        System.out.println(str.indexOf('o'));
+        System.out.println(str.lastIndexOf('o'));
+        System.out.println(str.substring(2));
+        System.out.println(str.substring(2,4));
+        System.out.println(str.equals("hello"));
+        System.out.println(str.equalsIgnoreCase("Hello"));
 
-        //自动转换
-        double conversionA = a;
-        System.out.println(conversionA);
-
-        //强制转换
-        int conversionB = (int)b;
-        System.out.println(conversionB);
-
-       //char类型转换为int类型
-
-        int conversionC = (int)c;
-        System.out.println(conversionC);
     }
 }
