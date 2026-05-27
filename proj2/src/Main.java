18d6e517d8ab719d98c152acf51ebfcb209c5ee9
taiyang-1 <2107502278@qq.com>
2026-05-27 11:33:42 +0800

    匿名内部类

--- proj2/src/Main.java
+++ proj2/src/Main.java
@@ -1,3 +1,7 @@
+import Interface.USB;
+import impl.UsbKeyboardImpl;
+import impl.UsbMouseImpl;
+
 /**
  * @author lyf
  */
@@ -18,8 +22,19 @@ public class Main {
                     System.out.println(t.getSalary());
                 }
             }
-
-
+        }
+        USB[] u = new USB[3];
+        u[0] = new UsbKeyboardImpl();
+        u[1] = new UsbMouseImpl();
+        u[2] = new USB() {
+            @Override
+            public void start() { System.out.println("U盘连接"); }
+            @Override
+            public void stop() { System.out.println("U盘断开"); }
+        };
+        for (int i = 0; i < u.length; i++) {
+            u[i].start();
+            u[i].stop();
         }
     }
 }
