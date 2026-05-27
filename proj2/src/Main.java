8978e153e85f42909afda9dba84cf5a0191b2494
taiyang-1 <2107502278@qq.com>
2026-05-27 10:54:35 +0800

    增加循环逻辑

--- proj2/src/Main.java
+++ proj2/src/Main.java
@@ -4,6 +4,22 @@
 public class Main {
     public static void main(String[] args) {
         Person stu = new Student("lyf", 20, "1");
-        stu.show();
+        Person[] p = new Person[5];
+        p[0] = stu;
+        p[1] = new Teacher("ll", 12, 5000);
+        for (int i = 0; i < p.length; i++) {
+            if (p[i] != null) {
+                p[i].show();
+                if (p[i] instanceof Student) {
+                    Student s = (Student) p[i];
+                    System.out.println(s.getStuId());
+                } else if (p[i] instanceof Teacher) {
+                    Teacher t = (Teacher) p[i];
+                    System.out.println(t.getSalary());
+                }
+            }
+
+
+        }
     }
-    }
+}
