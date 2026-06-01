2bb87009c8e46fb0f525c636e3b3f47e0d9fb65d
taiyang-1 <2107502278@qq.com>
2026-06-01 16:33:44 +0800

    变成乐观锁

--- Thread/src/TicketRunnable.java
+++ Thread/src/TicketRunnable.java
@@ -1,13 +1,38 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 卖票任务 — 三个线程共享这同一个对象
  */
 public class TicketRunnable implements Runnable {
     private int tickets = 10;   // 不加 static，靠共享同一个对象
-
+    Lock lock = new ReentrantLock();
     @Override
     public void run() {
+//        while (true) {
+//            synchronized (this) {        // this 就是锁
+//                if (tickets > 0) {
+//                    System.out.println(Thread.currentThread().getName()
+//                            + " 卖出第" + tickets + "张票");
+//                    tickets--;
+//                    try {
+//                        Thread.sleep(500);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                } else {
+//                    break;               // 没票了，退出
+//                }
+//            }
+//        }
+
+
+
+
         while (true) {
-            synchronized (this) {        // this 就是锁
+            lock.lock();
+        try {
+
                 if (tickets > 0) {
                     System.out.println(Thread.currentThread().getName()
                             + " 卖出第" + tickets + "张票");
@@ -18,8 +43,11 @@ public class TicketRunnable implements Runnable {
                         e.printStackTrace();
                     }
                 } else {
-                    break;               // 没票了，退出
+                    System.out.println("票卖完了");
+                    break;              // 没票了，退出
                 }
+            }finally{
+                lock.unlock();
             }
         }
     }
