ccfd0d35184225f1d042caf6a7a00e25ff5fbe5f
taiyang-1 <2107502278@qq.com>
2026-06-01 16:06:07 +0800

    加锁

--- Thread/src/TicketRunnable.java
+++ Thread/src/TicketRunnable.java
@@ -6,15 +6,21 @@ public class TicketRunnable implements Runnable {
 
     @Override
     public void run() {
-        while (tickets > 0) {
-            try {
-                Thread.sleep(100);    // 放大线程安全问题
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        while (true) {
+            synchronized (this) {        // this 就是锁
+                if (tickets > 0) {
+                    System.out.println(Thread.currentThread().getName()
+                            + " 卖出第" + tickets + "张票");
+                    tickets--;
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    break;               // 没票了，退出
+                }
             }
-            // 票号可能重复或出现负数 ← 这就是线程安全问题
-            System.out.println(Thread.currentThread().getName() + " 卖出第" + tickets + "张票");
-            tickets--;
         }
     }
 }
